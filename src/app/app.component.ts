b7cbe7c558e01014b8eedf31ddc67ecef0f59b23
Vens Kolev <v.kolev@alfatrex.com>
2023-02-09 00:28:17 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>Hello World!</h1>
+  <p>My first Angular App</p>`
 })
+
 export class AppComponent {
   title = 'my-first-angular01';
 }
